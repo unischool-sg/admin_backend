f6af45a81dd4d18b8701c6bbd8527b92a014f99d
tanahiro2010 <herentongkegu087@gmail.com>
2026-04-07 21:06:01 +0900

    feat: 新しいマイグレーションファイルを追加し、スキーマを更新

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-  schema: './src/schema.ts',
+  schema: './src/lib/schema.ts',
   out: './migrations',
   dialect: 'sqlite',
   driver: 'd1-http', // D1用ドライバー
